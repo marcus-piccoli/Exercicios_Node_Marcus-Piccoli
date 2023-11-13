9334332ebff08c581d2c559aced088262291d08c
marcus-piccoli <marcus.piccoli@gmail.com>
2023-11-13 16:51:54 -0300

    express_server teste

--- Express_Server/index.js
+++ Express_Server/index.js
@@ -1,23 +1,54 @@
-const express = require("express")
+const express = require("express") //O módulo express retorna uma função que instancia o express
 
-const app = express()
+const app = express() //A função express cria uma instância de todo o framework express em app
 
-//cria o servidor
+//Cria o Servidor com o express
 const porta = 3001
-app.listen(porta, function()){
-    console.log(`Servidor rodando na porta: ${porta}`);
-}
+app.listen(porta, function(){
+    console.log(`Servidor Rodando na porta ${porta}`);
+})
 
-app.get("/", function(req, resp){
+//Rotas da aplicação
+app.get("/", function (req, resp) {
+
+    //req.??? --> arquivo 
     resp.send(
         `<html>
             <head>
                 <meta charset="utf-8">
             </head>
-            <body> 
-                <h1>Teste Node Express /</h1>
+            <body>
+                <h1>teste node 1</h1>
             </body>
         </html>
-        `
+        ` 
+    )
+})
+
+app.get("/musicas", function(req, resp){
+    resp.send(
+        `<html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Listagem de Músicas</h1>
+            </body>
+        </html>
+        `   
+    )
+})
+
+app.get("/musica", function(req, resp){
+    resp.send(
+        `<html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Damn On You</h1>
+            </body>
+        </html>
+        `   
     )
 })
